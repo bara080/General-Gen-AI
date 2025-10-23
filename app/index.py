91e7d3720f347f6629b6b3e70d76ec3c19ed2d9d
Bara080 <baraahmad232@gmail.com>
2025-10-23 16:53:58 -0400

    Cleaned up index.py

--- app/index.py
+++ app/index.py
@@ -3,15 +3,9 @@
 import os
 import sys
 from dotenv import load_dotenv
-from agenticai import ai_app
-
-BASE_DIR = os.path.dirname(__file__)          
-SRC_DIR  = os.path.join(BASE_DIR, "src")     
-if SRC_DIR not in sys.path:
-    sys.path.insert(0, SRC_DIR)
+from agenticai import ai_app as graph_app
 
 
-# ... rest of your FastAPI code unchanged ...
 
 
 # -----------------------------------------------------------------------------
